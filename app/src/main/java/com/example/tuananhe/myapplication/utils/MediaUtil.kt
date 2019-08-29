10202a5adb0f777f1b784cdbc704066f90b2c47c
Nguyen Tuan Anh E <nguyen.tuan.anhe@sun-asterisk.com>
2019-08-29 18:17:19 +0700

    share video and image

--- app/src/main/java/com/example/tuananhe/myapplication/utils/MediaUtil.kt
+++ app/src/main/java/com/example/tuananhe/myapplication/utils/MediaUtil.kt
@@ -2,38 +2,10 @@ package com.example.tuananhe.myapplication.utils
 
 import android.text.format.DateUtils
 import java.io.File
-import java.util.concurrent.TimeUnit
 
 class MediaUtil {
 
     companion object {
-
-        fun getDuration(time: Long, isVerbose: Boolean): String {
-            val min: Long
-            val sec: Long
-            val hour: Long = TimeUnit.SECONDS.toHours(time)
-
-            if (hour == 0L) {
-                min = TimeUnit.SECONDS.toMinutes(time)
-                sec = time - min * 60
-            } else {
-                min = TimeUnit.SECONDS.toMinutes(time - hour * 60 * 60)
-                sec = time - hour * 60 * 60 - min * 60
-            }
-
-            val duration: String
-            duration = if (hour == 0L) {
-                String.format("%02d:%02d", min, sec)
-            } else {
-                if (isVerbose) {
-                    String.format("%02d:%02d:%02d", hour, min, sec)
-                } else {
-                    String.format("%02d:%02d", hour, min)
-                }
-            }
-
-            return duration
-        }
 
         fun getVideoDuration(durationInLong: Long): String =
             DateUtils.formatElapsedTime(durationInLong)
